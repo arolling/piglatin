dd09cd7d7ede0e7596856695ca736f5e20183a78
Abigail Rolling and torrence stratton <arolling@gmail.com>
2016-01-20 09:38:27 -0800

    write business logic for broadest specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 var pigLatin = function(word) {
-  if ( (!word.startsWith("i")) || (!word.startsWith("e")) || (!word.startsWith("a")) || (!word.startsWith("o")) || (!word.startsWith("u")) ) {
-  var firstLetter = word.slice(0,1);
-  var word = word.slice(1);
-  word += firstLetter;
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  for (var i = 0; i < 11; i++) {
+    var vowel = word.startsWith(vowels[i]);
+    if (vowel === true) {
+      break;
+    } else {
+      break;
+    }
+  }
+
+
+  if ( vowel === false ) {
+    var firstLetter = word.slice(0,1);
+    var word = word.slice(1);
+    word += firstLetter;
+    console.log(word);
   }
   return word + 'ay';
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,7 @@
 describe("pigLatin", function () {
   it("will add ay at the end of a word beginning with a vowel", function() {
-    expect(pigLatin("ice")).to.equal("iceay");
+    expect(pigLatin("assert")).to.equal("assertay");
+
   });
   it("will move the first consonant to the end of the word before adding ay", function() {
     expect(pigLatin("word")).to.equal("ordway");
