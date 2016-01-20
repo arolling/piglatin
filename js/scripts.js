d7cf58eee0c0556e2f45f3baeb350d6bcc2b66b7
Abigail Rolling and torrence stratton <arolling@gmail.com>
2016-01-20 10:27:53 -0800

    write working business logic for all cases except qu

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 var pigLatin = function(word) {
+  var position = findVowel(word);
+
+  var firstLetter = word.slice(0, position);
+  var word = word.slice(position);
+  word += firstLetter;
+  console.log(word);
+  return word + 'ay';
+}
+
+var findVowel = function(word) {
   var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  for (var i = 0; i < 11; i++) {
-    var vowel = word.startsWith(vowels[i]);
-    if (vowel === true) {
-      break;
-    } else {
-      break;
+  for (var position = 0; position < word.length; position++) {
+    for (var i = 0; i < 11; i++) {
+      var vowel = word.startsWith(vowels[i], position);
+      if (vowel === true) {
+        
+        console.log(vowel, position, word);
+        return position;
+      }
     }
   }
-
-
-  if ( vowel === false ) {
-    var firstLetter = word.slice(0,1);
-    var word = word.slice(1);
-    word += firstLetter;
-    console.log(word);
-  }
-  return word + 'ay';
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,19 @@
 describe("pigLatin", function () {
   it("will add ay at the end of a word beginning with a vowel", function() {
-    expect(pigLatin("assert")).to.equal("assertay");
-
+    expect(pigLatin("Assert")).to.equal("Assertay");
   });
+
   it("will move the first consonant to the end of the word before adding ay", function() {
     expect(pigLatin("word")).to.equal("ordway");
   });
 
+  it("will move the first group of consonants", function() {
+    expect(pigLatin("trade")).to.equal("adetray");
+  });
 
+  it("will include u when moving q", function() {
+    expect(pigLatin("quash")).to.equal("ashquay");
+  });
 
 
 });
